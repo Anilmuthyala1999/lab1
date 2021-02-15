6f1d245a09e8ab2299e887a6c5d4c9aa736b04ba
Anilmuthyala1999 <79087117+Anilmuthyala1999@users.noreply.github.com>
2021-02-15 17:35:46 +0530

    Add files via upload

---
+++ SavingsAccount.java
@@ -0,0 +1,18 @@
+package packagebank;
+
+
+public class SavingsAccount extends Account {
+	final double minimumBalance =500;
+	SavingsAccount(String name,float age,double initialBalance){
+		super(name,age,initialBalance);
+	}
+	
+	@Override
+	public void withdraw(double withdrawAmount) {
+		if(super.balance<=this.minimumBalance) {
+			System.out.println("Account Balance is below Minimum ");
+		}
+		else super.balance-=withdrawAmount;
+	}
+	
+}
